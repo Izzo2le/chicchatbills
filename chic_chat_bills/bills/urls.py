57df5b8d1f30c49f5ccb1ffb75431b934f379d88
Izzo2le <isobel.otoole@aim.com>
2024-08-15 17:18:41 +0000

    bills> urls.py added

---
+++ chic_chat_bills/bills/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('bills/', views.bill_list_view, name='bill_list'),
+    path('bills/create/', views.create_bill_view, name='create_bill'),
+    path('bills/<int:bill_id>/', views.bill_detail_view, name='bill_detail'),
+    path('bills/<int:bill_id>/add-item/', views.add_item_view, name='add_item'),
+]
