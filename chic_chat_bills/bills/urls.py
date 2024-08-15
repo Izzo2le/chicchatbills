0e89f590833b8298b4a3554314e22a71901d1123
Izzo2le <isobel.otoole@aim.com>
2024-08-15 17:42:46 +0000

    views bill list and bill detail added to urls.py

--- chic_chat_bills/bills/urls.py
+++ chic_chat_bills/bills/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('bills/create/', views.create_bill_view, name='create_bill'),
     path('bills/<int:bill_id>/', views.bill_detail_view, name='bill_detail'),
     path('bills/<int:bill_id>/add-item/', views.add_item_view, name='add_item'),
+    path('', views.bill_list, name='bill_list'),
+    path('bill/<int:bill_id>/', views.bill_detail, name='bill_detail'),
+]
 ]
