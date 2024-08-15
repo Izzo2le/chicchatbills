f69db7d27f4df49afebfe8d9348e2f1cfd82a7fc
Izzo2le <isobel.otoole@aim.com>
2024-08-15 17:51:23 +0000

    models.py update

--- chic_chat_bills/bills/models.py
+++ chic_chat_bills/bills/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Bill(models.Model):
+   
+    UID = models.CharField(max_length=100)
+  
+class Item(models.Model):
+ 
+    UID = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    numberOfSplits = models.IntegerField()
+    BillId = models.ForeignKey(Bill, on_delete=models.CASCADE, related_name='items')
+    pricePerSplit = models.DecimalField(max_digits=10, decimal_places=2)
+   
